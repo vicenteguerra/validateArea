7a9af7af7e6e4f9f107190b047e4393cde89e7ec
Vicente Guerra <vicenteguerrahdez@gmail.com>
2016-10-26 12:18:13 -0500

    Simple Landing page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Valid Place</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+
+    <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet">
+
+    <link href="<?php echo asset('/css/style.css') ?>" rel="stylesheet"/>
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
@@ -42,7 +46,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
+                    Valid Place
                 </a>
             </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
 
-                <div class="panel-body">
-                    
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="info-home col-md-8 col-md-offset-2 text-center">
+  <h1>Valid Place</h1>
+  <h2>Draw in a map your valid area (For any kind of services)</h2>
+  <h2>Get an Http request for validate a location (Verify if the coordinates are inside of the drawn polygon)</h2>
 </div>
+
+<div class="video-container">
+  <iframe class="background-video"
+  src="//player.vimeo.com/video/156188945?title=0&amp;byline=0&amp;portrait=0&amp;color=3a6774&amp;autoplay=1&amp;loop=1"
+  frameborder="0" height="100%" width="100%" webkitallowfullscreen
+  mozallowfullscreen allowfullscreen>
+  </iframe>
+</div>
+
+
+<footer >
+    <div class="col-lg-12">
+      <div class="col-md-8">
+        <a href="http://twitter.com/IngHucruz">@IngHucruz</a> | <a href="https://twitter.com/ok_atomic">@ok_atomic</a>
+      </div>
+      <div class="col-md-4 text-right">
+        <a href="http://yamblet.com/">2016 Yamblet</a>
+      </div>
+    </div>
+</footer>
+
 @endsection
